59d03e66e1dafbbf219cae6a88b84e07ac23e7cd
Youssef samih <youssef.samih@4d.com>
2021-04-04 18:11:20 +0100

    feat: add theme provider and global style

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,14 +1,24 @@
+import 'react-phone-input-2/lib/style.css';
+import 'theme/fonts.css';
+import theme from 'theme/config';
+import GlobalStyle from 'theme/global-style';
 import { AppProps } from 'next/app';
 import { Router } from 'next/router';
 import nProgress from 'nprogress';
+import { ThemeProvider } from 'styled-components';
 
 Router.events.on('routeChangeStart', () => nProgress.start());
-
 Router.events.on('routeChangeComplete', () => nProgress.done());
 Router.events.on('routeChangeError', () => nProgress.done());
-
 const App = ({ Component, pageProps }: AppProps) => {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <GlobalStyle />
+      <ThemeProvider theme={theme}>
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </>
+  );
 };
 
 export default App;
